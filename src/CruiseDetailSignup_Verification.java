8086a3b7f785acbf41c91d6641bf9a7edc0a3339
Daman1231 <daman.singh00786@gmail.com>
2022-10-28 23:46:43 -0400

    cruise_Sign_up

---
+++ src/CruiseDetailSignup_Verification.java
@@ -0,0 +1,50 @@
+
+public class CruiseDetailSignup_Verification {
+
+	String emailID;
+	int password;
+	String fullName;
+	String phoneNumber;
+
+	public CruiseDetailSignup_Verification(String email, int pass, String fullNam, String mobileNum) {
+		emailID = email;
+		password = pass;
+		fullName = fullNam;
+		phoneNumber = mobileNum;
+
+	}
+
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		if(emailID.equals(this.emailID)) {
+			System.out.println("Valid Email");
+		}
+		else {
+			System.out.println("Invalid Email");
+		}
+		this.emailID = emailID;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
+}
